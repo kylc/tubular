f276d6540fed0d4efb9814a82813065dcecbcf83
Kyle Cesare <kcesare@gmail.com>
2013-05-11 16:00:09 -0700

    Reorganize Tracker module.

--- lib/tubular/tracker.rb
+++ lib/tubular/tracker.rb
@@ -2,45 +2,66 @@ require 'net/http'
 require 'uri'
 
 module Tubular
-  class Tracker
-    attr_reader :torrent
-
-    def initialize(torrent)
-      @torrent = torrent
-    end
-
-    def request
-      params = {
-        info_hash: @torrent.info_hash,
-        peer_id: "aaaaaaaaaaaaaaaaaaaa",
-        ip: "0.0.0.0",
-        port: 5555,
-        uploaded: 0,
-        downloaded: 0,
-        left: 0,
-        event: :started,
-        compact: 1
+  module Tracker
+    class Tracker
+      DEFAULT_PARAMS = {
+          info_hash: nil,
+          peer_id: "aaaaaaaaaaaaaaaaaaaa",
+          ip: "0.0.0.0",
+          port: 5555,
+          uploaded: 0,
+          downloaded: 0,
+          left: 0,
+          event: :started,
+          compact: 1
       }
 
-      uri = URI(@torrent['announce'])
-      uri.query = URI.encode_www_form(params)
+      attr_reader :torrent, :params
 
-      resp = Net::HTTP.get_response(uri)
-
-      Bencode::parse_from_string(resp.body)
-    end
-
-    def decode_peers(binary)
-      peers = []
-
-      binary.chars.to_a.each_slice(6) do |peer|
-        ip = peer[0..3].join.unpack('CCCC').join('.')
-        port = peer[4..5].join.unpack('n').first
-
-        peers << [ip, port].join(':')
+      def initialize(torrent, params={})
+        @torrent = torrent
+        @params = DEFAULT_PARAMS.merge(params)
+        @params[:info_hash] = @torrent.info_hash
       end
 
-      peers
+      def request
+        resp = Net::HTTP.get_response(request_url)
+
+        Response.new(Bencode::parse_from_string(resp.body))
+      end
+
+      private
+
+      def request_url
+        url = URI(@torrent['announce'])
+        url.query = URI.encode_www_form(@params)
+        url
+      end
+    end
+
+    class Response
+      attr_reader :body
+
+      def initialize(body)
+        @body = body
+      end
+
+      def success?
+        !body.has_key?('failure reason')
+      end
+
+      def peers
+        peers = []
+
+        body['peers'].chars.to_a.each_slice(6) do |peer|
+          ip = peer[0..3].join.unpack('CCCC').join('.')
+          port = peer[4..5].join.unpack('n').first
+
+          peers << [ip, port].join(':')
+        end
+
+        peers
+      end
     end
   end
 end
