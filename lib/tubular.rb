0223406e65d97413d9769ee0f7742d9963448a6c
Kyle Cesare <kcesare@gmail.com>
2013-05-12 17:01:07 -0700

    Work on big picture stuff.

--- lib/tubular.rb
+++ lib/tubular.rb
@@ -1,5 +1,11 @@
 require_relative 'tubular/bencode'
 require_relative 'tubular/torrent'
+require_relative 'tubular/tracker'
 
 module Tubular
+  def self.download(torrent_file)
+    torrent = Torrent.open(torrent_file)
+    tracker = Tracker::Tracker.new(torrent)
+    resp = tracker.perform
+  end
 end
