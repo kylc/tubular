cfe40c2c795a5b1dd543e35a7d7436dcc3888e90
Kyle Cesare <kcesare@gmail.com>
2013-05-16 19:02:05 -0700

    Record incoming piece data.

--- lib/tubular/peer.rb
+++ lib/tubular/peer.rb
@@ -24,6 +24,14 @@ module Tubular
         message = recv_message
 
         Tubular.logger.debug "Message: #{message}"
+
+        case message.type
+        when :bitfield
+          @piece_map = message.payload[:bitfield]
+          p @piece_map.length
+        when :have
+          @piece_map[message.payload[:piece_index]] = true
+        end
       end
     end
   end
