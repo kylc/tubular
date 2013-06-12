fa0b1da6d20ea33f22526124c4d08503ac82fd05
Kyle Cesare <kcesare@gmail.com>
2013-06-11 21:32:16 -0700

    Remove reference to peer ID from environment.

--- lib/tubular/environment.rb
+++ lib/tubular/environment.rb
@@ -6,9 +6,6 @@ module Tubular
     # The torrent we are currently operating upon.
     attr_accessor :torrent
 
-    # Our peer ID.
-    attr_accessor :peer_id
-
     # The local file.
     attr_accessor :local
   end
